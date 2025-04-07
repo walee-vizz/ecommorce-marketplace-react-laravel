d0830a5b379e658014669585fd7ff59f4686d45f
Waleed khan <muhammadwaleedkhan052.com>
2025-04-07 23:03:46 +0500

    Stripe package installed, Orders tables created and enum class for order status

---
+++ app/Enums/OrderStatusEnum.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+
+enum OrderStatusEnum: String implements HasColor, HasIcon, HasLabel
+{
+
+    case Draft = 'draft';
+
+    case Pending = 'pending';
+
+    case Confirmed = 'confirmed';
+
+    case Processing = 'processing';
+
+    case Shipped = 'shipped';
+
+    case OutForDelivery = 'out for delivery';
+
+    case Delivered = 'delivered';
+
+    case Paid = 'paid';
+
+    case Refunded = 'refunded';
+
+    case Cancelled = 'cancelled';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Draft => 'Draft',
+            self::Pending => 'Pending',
+            self::Confirmed => 'Confirmed',
+            self::Processing => 'Processing',
+            self::Shipped => 'Shipped',
+            self::OutForDelivery => 'Out For Delivery',
+            self::Delivered => 'Delivered',
+            self::Refunded => 'Refunded',
+            self::Cancelled => 'Cancelled',
+        };
+    }
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::Draft => 'info',
+            self::Pending => 'primary',
+            self::Confirmed => 'info',
+            self::Processing => 'warning',
+            self::Shipped, self::OutForDelivery, self::Delivered => 'success',
+            self::Cancelled, self::Refunded => 'danger',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::Draft            => 'heroicon-m-sparkles',       // Icon for new items
+            self::Pending     => 'heroicon-m-arrow-path',      // Icon for in-progress status
+            self::Confirmed      => 'heroicon-m-check-circle',    // Confirmation icon
+            self::Processing     => 'heroicon-m-arrow-path',      // Icon for in-progress status
+            self::Shipped        => 'heroicon-m-truck',           // Shipping icon
+            self::OutForDelivery => 'heroicon-m-clock',           // Delivery timing icon
+            self::Delivered      => 'heroicon-m-check-badge',     // Delivered/success icon
+            self::Cancelled      => 'heroicon-m-x-circle',        // Cancelled icon
+            self::Refunded       => 'heroicon-m-currency-dollar', // Currency dollar icon for refunded status
+        };
+    }
+}
---
+++ database/migrations/2025_04_07_173119_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total_price', 20, 4);
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(User::class, 'vendor_user_id');
+            $table->string('status')->default('pending');
+            $table->string('stripe_session_id')->nullable();
+            $table->decimal('online_payment_commission', 20, 4)->nullable();
+            $table->decimal('website_commission', 20, 4)->nullable();
+            $table->decimal('vendor_subtotal', 20, 4)->nullable();
+            $table->string('payment_intent')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained('products');
+            $table->decimal('price', 20, 4);
+            $table->integer('quantity');
+            $table->json('variation_type_option_ids')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+        Schema::dropIfExists('orders');
+    }
+};
